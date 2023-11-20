54ed0b4f86aeddf92a5ae9c63c94e5acd153b449
b.r.o.t.e.m.oo@gmail.com <b.r.o.t.e.m.oo@gmail.com>
2023-11-20 22:56:57 +0200

    Добавьте файлы проекта.

---
+++ lab4.2Popov/Program.cs
@@ -0,0 +1,47 @@
+﻿//средний 6 
+Random random = new Random();
+int n = 5;
+int[,] A = new int[n, n];
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        A[i, j] = random.Next(1, 100);
+        Console.Write(A[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+int minElement = A[0, 0];
+int min = 0;
+for (int i = 1; i < n; i++)
+{
+    if (A[i, i] < minElement)
+    {
+        minElement = A[i, i];
+        min = i;
+    }
+}
+
+int[,] B = new int[n - 1, n];
+for (int i = 0, k = 0; i < n; i++)
+{
+    if (i != min)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            B[k, j] = A[i, j];
+        }
+        k++;
+    }
+}
+
+Console.WriteLine("матрица B без строки:");
+for (int i = 0; i < n - 1; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(B[i, j] + " ");
+    }
+    Console.WriteLine();
+}
